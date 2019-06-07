da4fedbbda45fb7adfb349caa6fbfd836bbdf86c
SATHYA RAJESH POLICHERLA <U8KD@eurofins.com>
2019-06-07 12:40:21 +0530

    Updated first-module in app

---
+++ DemoProjectAngular/src/app/first-module/first-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import { HttpClient } from 'selenium-webdriver/http';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    HttpClient
+  ]
+})
+export class FirstModuleModule { }
